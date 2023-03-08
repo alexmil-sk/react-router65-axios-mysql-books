465ba7a417eb0b6f9fc89fc43198e3cdc553e5c7
alexmil-sk <alexmilweb@gmail.com>
2023-03-08 20:49:16 +0100

    add Dockerfile

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 dotenv.config();
-import {
-	readCatalog,
-	deleteItem
-} from './utils/dbActions.js';
 import dbConnect from './utils/dbConnection.js';
+import {
+	readCatalog, updateBook, getBook, addBook, deleteBook
+} from "./utils/dbActions.js";
 
 
 const AXIOS_PORT = process.env.AXIOS_PORT;
@@ -20,70 +19,16 @@ app.get("/", (req, res) => {
 	res.json('Its backend');
 });
 
-app.get("/books", (req, res) => {
-	const q = "SELECT * FROM books";
 
-	dbConnect.query(q, (error, data) => {
-		if (error) return res.json(error);
-		return res.json(data);
-	});
-});
+app.get("/books", readCatalog);
 
+app.put("/books/:id", updateBook);
 
-app.put("/books/:id", (req, res) => {
-	const q = "UPDATE books SET `id` = ?, `title` = ?, `description` = ?, `price` = ?, `cover` = ? WHERE id = ?";
-	const values = [
-		req.body.id,
-		req.body.title,
-		req.body.description,
-		req.body.price,
-		req.body.cover,
-	];
-	const bookId = req.params.id;
+app.get("/books/:id", getBook);
 
-	dbConnect.query(q, [...values, bookId], (error, info) => {
-		if (error) return res.json(error);
-		return res.json("Book has been updated successfully");
-	})
-});
+app.post("/books", addBook);
 
-app.get("/books/:id", (req, res) => {
-	const q = "SELECT * FROM books WHERE id = ?";
-	const id = req.params.id;
-
-	dbConnect.query(q, [id], (error, data) => {
-		if (error) return res.json(error);
-		console.log(data);
-		return res.json(data);
-	})
-});
-
-app.post("/books", (req, res) => {
-
-	const q = "INSERT INTO books (`id`, `title`, `description`, `price`, `cover`) VALUES (?)";
-	const values = [
-		req.body.id,
-		req.body.title,
-		req.body.description,
-		req.body.price,
-		req.body.cover,
-	];
-
-	dbConnect.query(q, [values], (error, info) => {
-		if (error) return res.json(error);
-		return res.json("Book has been created successfully!");
-	})
-});
-
-app.delete("/books/:id", (req, res) => {
-	const q = "DELETE FROM books WHERE id = ?";
-	const bookId = req.params.id
-
-	dbConnect.query(q, [bookId], (error, info) => {
-		if (error) return res.json(error);
-		return res.json("Book has been deleted successfully!");
-	})
-});
+app.delete("/books/:id", deleteBook);
 
 
 app.listen(AXIOS_PORT, () => {
--- backend/utils/dbActions.js
+++ backend/utils/dbActions.js
@@ -1,38 +1,69 @@
 import dbConnect from './dbConnection.js';
 
-const readCatalog = () => {
-	new Promise((resolve, reject) => 
-		dbConnect.getConnection((err, connection) => {
-			if (err) return reject(err);
-			connection.query(
-				"SELECT * FROM books",
-				(err, results) => {
-					if (err) return reject(err);
-					resolve(results);
-				}
-			)
-			connection.release();
-		})
-	);
-}
 
-const deleteItem = (id) => {
-	newPromise((resolve, reject) => {
-		dbConnect.getConnection((err, connection) => {
-			if (err) return reject(err);
-			connection.query(
-				`DELETE FROM books WHERE id = ${id}`,
-				(err, result) => {
-					if (err) return reject(err)
-					console.log("Element has been deleted successfully!");
-					resolve(result);
-				}
-			)
-			connection.release();
-		})
+const readCatalog = (req, res) => {
+	const q = "SELECT * FROM books";
+	dbConnect.query(q, (error, data) => {
+		if (error) return res.json(error);
+		return res.json(data);
+	});
+};
+
+const updateBook = (req, res) => {
+	const q = "UPDATE books SET `id` = ?, `title` = ?, `description` = ?, `price` = ?, `cover` = ? WHERE id = ?";
+	const values = [
+		req.body.id,
+		req.body.title,
+		req.body.description,
+		req.body.price,
+		req.body.cover,
+	];
+	const bookId = req.params.id;
+
+	dbConnect.query(q, [...values, bookId], (error, info) => {
+		if (error) return res.json(error);
+		return res.json("Book has been updated successfully");
+	})
+};
+
+const getBook = (req, res) => {
+	const q = "SELECT * FROM books WHERE id = ?";
+	const id = req.params.id;
+
+	dbConnect.query(q, [id], (error, data) => {
+		if (error) return res.json(error);
+		console.log(data);
+		return res.json(data);
+	})
+};
+
+const addBook = (req, res) => {
+
+	const q = "INSERT INTO books (`id`, `title`, `description`, `price`, `cover`) VALUES (?)";
+	const values = [
+		req.body.id,
+		req.body.title,
+		req.body.description,
+		req.body.price,
+		req.body.cover,
+	];
+
+	dbConnect.query(q, [values], (error, info) => {
+		if (error) return res.json(error);
+		return res.json("Book has been created successfully!");
+	})
+};
+
+const deleteBook = (req, res) => {
+	const q = "DELETE FROM books WHERE id = ?";
+	const bookId = req.params.id
+
+	dbConnect.query(q, [bookId], (error, info) => {
+		if (error) return res.json(error);
+		return res.json("Book has been deleted successfully!");
 	})
 }
 
 export {
-	readCatalog, deleteItem
-}
+	readCatalog, updateBook, getBook, addBook, deleteBook
+};
